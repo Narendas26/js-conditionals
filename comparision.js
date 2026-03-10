5fac0fbee85dd31c3c2d9833437331b5f81879f7
Narendas26 <naosoft1419@gmail.com>
2026-03-10 20:44:35 +0600

    Javascript Conditional Operator

---
+++ comparision.js
@@ -0,0 +1,29 @@
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(10 == 10);
+console.log(10 >= 10);
+console.log(15 >= 10);
+console.log(5 >= 10);
+console.log(5 <= 10);
+console.log(10 <= 10);
+console.log(10 != 10);
+
+var guavaPrice = 40;
+var applePrice = 250;
+
+console.log(guavaPrice > applePrice);
+console.log(applePrice < guavaPrice);
+
+/*
+    ==
+    ===
+*/
+
+console.log(10 == 10); //Here check data only
+console.log(10 == '10'); //Here Check data only
+console.log(10 === '10'); //Here Check data and data type here second data is string thats why it show false
+console.log(10 != '10');
+console.log(10 !== '10');
+
+
---
+++ variable.js
@@ -0,0 +1,32 @@
+// Variable => container
+
+//Number Variable
+var sunglassPrice = 80;
+
+//String variable
+var subject = "CSE";
+
+//boolean Variable
+var passed = true;
+
+/*
+  Naming Convention
+  1. single word
+  2. no quote
+  3. no gap or dash
+  4. can not start with a number
+  5. can not be a keyword
+  6. prefer camelCase
+*/
+
+var lastExamResult = 89;
+
+/*
+   Operations:
+   1. +
+   2. -
+   3. *
+   4. /
+   5. %
+   6. +=, -=, *=, /=, %= 
+*/
