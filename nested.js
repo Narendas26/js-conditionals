be6873a29cbf052498bd64dbf586538dfbc15b95
Narendas26 <naosoft1419@gmail.com>
2026-03-12 21:17:17 +0600

    nested Condition

---
+++ nested.js
@@ -0,0 +1,18 @@
+const money = 0;
+
+if(money > 300){
+    console.log("Bro you are rich!!");
+}
+else{
+    if(money > 100){
+        console.log("You are not rich nor poor!!");
+    }
+    else{
+        if(money > 0){
+            console.log("Dost kola kha calcium bara");
+        }
+        else{
+            console.log("Ami Tor Friend na");
+        }
+    }
+}
