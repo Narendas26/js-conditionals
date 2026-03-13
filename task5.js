574409f0e9044cfd982586f700377f6b07e85557
Narendas26 <naosoft1419@gmail.com>
2026-03-14 02:21:00 +0600

    task 5

---
+++ task5.js
@@ -0,0 +1,13 @@
+var num1 = 400;
+var num2 = 450;
+
+var result;
+
+if(num1 > num2){
+    result = num1 * num1;
+    console.log("When num1 is larger than num2",result);
+}
+else{
+    result = num1 + num2;
+    console.log("When num1 is not larger than num2",result);
+}
